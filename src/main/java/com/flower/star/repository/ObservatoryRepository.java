6d056ffd1e7fd03b0162a784e8158d1bad7c3a56
hosnn <soghtmd@gmail.com>
2024-06-27 10:48:07 +0900

    천문대레포 오류 수정

--- src/main/java/com/flower/star/repository/ObservatoryRepository.java
+++ src/main/java/com/flower/star/repository/ObservatoryRepository.java
@@ -3,7 +3,9 @@ package com.flower.star.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.flower.star.entity.Observatory;
+
 @Repository
-public interface ObservatoryRepository extends JpaRepository<ObservatoryRepository, Integer> {
+public interface ObservatoryRepository extends JpaRepository<Observatory, Integer> {
 
 }
